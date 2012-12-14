daa5844c5b8e5b9845c53a45f22564e94d066c4c
Jan Stępień <jstepien@users.sourceforge.net>
2012-12-14 14:30:55 +0100

    Use select and ignore SIGPIPE when decoding width

--- libpkp.c
+++ libpkp.c
@@ -6,16 +6,21 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/select.h>
 #include "build/config.h"
 #include "pkp.h"
 
+static int make_non_blocking(int fd) {
+  int flags = fcntl(fd, F_GETFL, 0);
+  return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
 static pid_t in_out_popen(const char *cmd, int descriptors[2]) {
   pid_t pid;
-  int to[2], from[2], flags;
+  int to[2], from[2];
   pipe(to);
   pipe(from);
-  flags = fcntl(from[0], F_GETFL, 0);
-  if (fcntl(from[0], F_SETFL, flags | O_NONBLOCK) < 0)
+  if (make_non_blocking(to[1]) < 0)
     return -1;
   if ((pid = fork()) > 0) {
     close(to[0]);
@@ -37,34 +42,54 @@ static pid_t in_out_popen(const char *cmd, int descriptors[2]) {
 }
 
 static int run(const char *cmd, char *out, size_t nout, char *in, size_t nin) {
-  int fds[2], nread, nwritten;
+  int fds[2], maxfd, retval;
   char *cur_out = out, *cur_in = in;
   pid_t pid = in_out_popen(cmd, fds);
   if (pid < 0)
     return -1;
+  fd_set rd, wr;
+  maxfd = fds[0] > fds[1] ? fds[0] : fds[1];
   while (1) {
-    if (cur_in - nin < in) {
-      nwritten = write(fds[1], in, nin - (cur_in - in));
-      if (nwritten < 0)
-        return -2;
-      else
-        cur_in += nwritten;
-    } else {
-      close(fds[1]);
-    }
-    nread = read(fds[0], cur_out, nout - (cur_out - out));
-    if (nread < 0) {
-      if (errno == EAGAIN)
-        continue;
-      return -3;
-    } else if (nread == 0)
+    int nread, nwritten, selected;
+    do {
+      FD_ZERO(&rd);
+      FD_ZERO(&wr);
+      if (cur_in - nin < in)
+        FD_SET(fds[1], &wr);
+      FD_SET(fds[0], &rd);
+      selected = select(maxfd + 1, &rd, &wr, 0, 0);
+    } while (selected == -1 && errno == EINTR);
+    if (selected == -1) {
+      retval = -7;
       break;
-    else
+    }
+    if (FD_ISSET(fds[0], &rd)) {
+      nread = read(fds[0], cur_out, nout - (cur_out - out));
+      if (nread < 0) {
+        retval = -3;
+        break;
+      } else if (nread == 0) {
+        retval = cur_out - out;
+        break;
+      }
       cur_out += nread;
+    }
+    if (FD_ISSET(fds[1], &wr)) {
+      nwritten = write(fds[1], cur_in, nin - (cur_in - in));
+      if (nwritten < 0) {
+        retval = -2;
+        break;
+      }
+      cur_in += nwritten;
+      if (cur_in - nin >= in)
+        close(fds[1]);
+    }
   }
+  close(fds[1]);
+  close(fds[0]);
   if (pid != wait(0))
     return -4;
-  return cur_out - out;
+  return retval;
 }
 
 int pkp_compress(char *out, size_t nout, char *in, size_t nin, int width) {
@@ -79,8 +104,9 @@ int pkp_compress(char *out, size_t nout, char *in, size_t nin, int width) {
 }
 
 int pkp_decompress(char *out, size_t nout, char *in, size_t nin, int* width) {
-  if (run("unxz | head -c 2", out, nout, in, nin) < 0)
-    return -5;
+  signal(SIGPIPE, SIG_IGN);
+  run("unxz | head -c 2", out, nout, in, nin);
+  signal(SIGPIPE, SIG_DFL);
   *width = ((unsigned char) out[0] << 8) + (unsigned char) out[1];
   return run("unxz |" PREFIX "/lib/" APPNAME "/Decode", out, nout, in, nin);
 }
